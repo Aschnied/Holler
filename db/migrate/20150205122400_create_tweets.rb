45ba2d7b1e6d08469f872eb63d593fc2f746f412
Andrew Schnieder <andrewschnieder@gmail.com>
2015-02-05 13:23:48 -0800

    Completing migrations

--- db/migrate/20150205122400_create_tweets.rb
+++ db/migrate/20150205122400_create_tweets.rb
@@ -1,1 +1,11 @@
-20150205122400_create_tweets.rb
+class CreateHollers < ActiveRecord::Migration
+  def change
+    create_table :hollers do |t|
+      t.string :content
+      t.integer :user_id
+      t.string  :orig_author
+
+      t.timestamps
+    end
+  end
+end
