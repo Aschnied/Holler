17789ceaf13c56af95e7597463f26523b952f062
Shambhavi Sachid <shambu1985@gmail.com>
2015-02-05 20:57:15 -0800

    Saleh is the king of all internet

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
   has_many :follows, foreign_key: :follower_id
   has_many :follows, foreign_key: :followed_id
 
-  validates :user_handle, :full_name, :email, :password, presence: true
+  # validates :user_handle, :full_name, :email, :password, presence: true
 end
